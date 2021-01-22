ce689580f2434b7cbe2fc4b35cb2ee6746e3da2f
fait@orkasolutions.cz <fait@orkasolutions.cz>
2021-01-22 23:32:06 +0100

    Funkcni netmiko

--- netmiko/netmiko_prompt_global_config.py
+++ netmiko/netmiko_prompt_global_config.py
@@ -3,7 +3,7 @@ tplink_t1600 = {
     'device_type': 'tplink_jetstream',
     'host':   '192.168.10.252',
     'username': 'admin',
-    'password': 'admin',
+    'password': 'piatkoUZneni',
     'port': 22,
     'verbose': True,
 }
@@ -11,17 +11,18 @@ connection = ConnectHandler(**tplink_t1600)
 
 # getting the router's prompt
 prompt = connection.find_prompt()
-# if '>' in prompt:
-#        connection.enable(cmd='enable')   # entering the enable mode
+if '>' in prompt:
+       connection.enable(cmd='enable-admin')   # entering the enable mode
 
-output = connection.send_command('sh run')
+output = connection.send_command('show mac-vlan all ')
 print(output)
 
 if not connection.check_config_mode(): # returns True if it's already in the global config mode
        connection.config_mode()  # entering the global config mode
 
-# print(connection.check_config_mode())
-connection.send_command('username u3 secret cisco')
+print(connection.check_config_mode())
+connection.send_command('user name u4 password cisco')
+# connection.send_command('save')
 
 connection.exit_config_mode()  # exiting the global config mode
 print(connection.check_config_mode())
--- netmiko/netmiko_test.py
+++ netmiko/netmiko_test.py
@@ -1,18 +1,16 @@
 from netmiko import ConnectHandler
 
-# connection = Netmiko(host='192.168.10.252', port='22')
-
 tplink_t1600 = {
     'device_type': 'tplink_jetstream',
     'host':   '192.168.10.252',
     'username': 'admin',
-    'password': 'admin',
+    'password': 'piatkoUZneni',
     'port': 22,
     'verbose': True,
 }
 
 connection = ConnectHandler(**tplink_t1600)
-output = connection.send_command('show vlan brief')
+output = connection.send_command('show mac-vlan all')
 
 print(output)
 
