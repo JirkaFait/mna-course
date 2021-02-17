b49e95a86d966d872c50b256175cc0a8bf8f408d
jiri.fait <fait@orkasolutions.cz>
2021-02-17 18:19:33 +0100

    Testovani verze 4

--- netmiko/netmiko_prompt_global_config.py
+++ netmiko/netmiko_prompt_global_config.py
@@ -28,8 +28,12 @@ if not connection.check_config_mode():  # returns True if it's already in the gl
 print(connection.check_config_mode())
 cmd = 'mac-vlan mac-address b4:f5:24:0a:93:ed vlan 5 description TECHNIK'
 output = connection.send_command(cmd)
-# output = connection.send_command('user name u6 password cisco')
 print(output)
+
+# cmd = 'mac-vlan mac-address aa:bb:cc:dd:ee:ff vlan 3 description POKUS'
+# output = connection.send_command(cmd)
+# # output = connection.send_command('user name u6 password cisco')
+# print(output)
 
 connection.exit_config_mode()  # exiting the global config mode
 print(connection.check_config_mode())
