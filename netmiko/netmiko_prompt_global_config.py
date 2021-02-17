82a860889d2f3f1a6c81f8e343b635a8050242ae
fait@orkasolutions.cz <fait@orkasolutions.cz>
2021-02-17 18:38:36 +0100

    pridano definice connect str

--- netmiko/netmiko_prompt_global_config.py
+++ netmiko/netmiko_prompt_global_config.py
@@ -1,23 +1,30 @@
 from netmiko import ConnectHandler
 import logging
+
 logging.basicConfig(filename='test.log', level=logging.DEBUG)
 loger = logging.getLogger("netmiko")
 
-tplink_t1600 = {
-    'device_type': 'tplink_jetstream',
-    'host': '10.40.1.10',
-    'username': 'admin',
-    'password': 'piatkovinatojedrina',
-    'port': 22,
-    'verbose': True,
-}
-connection = ConnectHandler(**tplink_t1600)
+
+def gen_tplink_connect_opt(host, port, password):
+    tplink_t1600 = {
+        'device_type': 'tplink_jetstream',
+        'host': host,
+        'username': 'admin',
+        'password': password,
+        'port': port,
+        'verbose': True,
+    }
+    return tplink_t1600
+
+
+con_str = gen_tplink_connect_opt('10.40.1.10', 22, 'piatkovinatojedrina')
+connection = ConnectHandler(**con_str)
 
 # getting the router's prompt
 prompt = connection.find_prompt()
 print(prompt)
 if '>' in prompt:
-     connection.enable()  # entering the enable mode
+    connection.enable()  # entering the enable mode
 
 output = connection.send_command('show mac-vlan mac-address b4:f5:24:0a:93:ed')
 print(output)
